790e0cf212cced0c10011af460b98b026b258a0e
HackerYijia <115096818+HackerYijia@users.noreply.github.com>
2023-05-22 20:24:11 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,18 @@
+{ pkgs }: {
+    deps = [
+        pkgs.rsync
+        pkgs.busybox
+        pkgs.tmux
+        pkgs.bashInteractive
+        pkgs.man
+        pkgs.jq
+        pkgs.ffmpeg
+        pkgs.jre8
+        pkgs.jdk11
+        pkgs.git
+        pkgs.tmux
+        pkgs.wget
+        pkgs.curl
+        pkgs.dialog
+    ];
+}
